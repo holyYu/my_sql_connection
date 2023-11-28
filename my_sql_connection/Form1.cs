fdd639073075b71089d2e7e6616ffc6e43238285
Admin <Admin@DESKTOP-R1T2R6B>
2023-11-28 23:37:21 +0800

    Add project files.

---
+++ my_sql_connection/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using my_sql_connection.myclass;
+
+namespace my_sql_connection
+{
+    public partial class Form1 : Form
+    {
+      class_connection con = new class_connection();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.connectdb.Open();
+                label1.Text = "You are Connected to the MySql Database!";
+                label1.ForeColor = Color.Green;
+                con.connectdb.Close();
+            }
+            catch (Exception)
+            {
+                label1.Text = "Failed to Connect to MySql Database!";
+                label1.ForeColor = Color.Red;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ my_sql_connection/myclass/class_connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace my_sql_connection.myclass
+{
+    internal class class_connection
+    {
+        public MySqlConnection connectdb;
+
+        public class_connection()
+        {
+            string host = "localhost";
+            string database = "studentdb";
+            string username = "root";
+            string password = "";
+            string port = "3306";
+
+            string connection_string = "datasource =" + host + ";database=" + database + "; port=" + port + "; username=" + username + ";" +
+                "password=" + password + "; SslMode =none;";
+
+            connectdb = new MySqlConnection(connection_string);
+        }
+    }
+}
